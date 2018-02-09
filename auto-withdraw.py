81272f835329540b9b7914776129b2f7086595f2
Jørn Sandvik Nilsson <joern.nilsson@gmail.com>
2018-02-09 14:31:49 +0100

    extracts nicehash site api to a module

--- auto-withdraw.py
+++ auto-withdraw.py
@@ -3,6 +3,7 @@ import logging
 import http.cookiejar
 import imaplib
 import re
+import nicehash_site_api
 
 spin_wait = 60*10
 coinbase_account = os.getenv('COINBASE_ACCOUNT')
@@ -23,108 +24,33 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-import pickle
-def save_cookies(requests_cookiejar, filename):
-    with open(filename, 'wb') as f:
-        pickle.dump(requests_cookiejar, f)
-
-def load_cookies(filename):
-    with open(filename, 'rb') as f:
-        return pickle.load(f)
-
-class NicehashException(Exception):
-    def __init__(self, response):
-        self.response = response
-        self.status_code = response.status_code
-    def __str__(self):
-        return 'NicehashException: %s %s' % (str(self.status_code), self.response.text)
-
-class NicehashAuthException(NicehashException): pass
-class NicehashServerErrorException(NicehashException): pass
-class NicehashClientErrorException(NicehashException): pass
-class NicehashUnknownErrorException(NicehashException): pass
-
-def verifyResponse(response):
-    if (response.status_code >= 500):
-        raise NicehashServerErrorException(response)
-    elif(response.status_code >= 400):
-        raise NicehashAuthException(response)
-
-    data = response.json()
-    if ("status_code" in data and data["status_code"] >= 500):
-        raise NicehashServerErrorException(response)
-    elif("status_code" in data and data["status_code"] >= 400):
-        raise NicehashAuthException(response)
-    elif("error_code" in data and int(data["error_code"]) > 0):
-        raise NicehashClientErrorException(response)
-    elif("error" in data and int(data["error"]) < 0):
-        raise NicehashServerErrorException(response)
-
-
-
-
 def spin():
 
     # Load cookies
     cj = http.cookiejar.MozillaCookieJar(jar)
     cj.load(ignore_expires=True)
+    cookies = {c.name:c.value for c in cj}
     #print(cj)
 
-    headers = {
-        'DNT': '1',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en-US,en;q=0.9,nb;q=0.8,sv;q=0.7',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.52 Safari/537.36',
-        'Accept': '*/*',
-        'Referer': 'https://www.nicehash.com/wallet',
-        'Connection': 'keep-alive',
-    }
+    api = nicehash_site_api.Client(cookies)
+    wallet = api.wallet()
 
-    params = (
-        ('with', 'transactions,deposits,withdraws,withdraw_requests'),
-    )
-    #print(load_cookies(jar))
-    cd = {c.name:c.value for c in cj}
-    #print({c.name:c.value for c in cj})
-    #return
-    response = requests.get('https://www.nicehash.com/siteapi/user/wallet', headers=headers, params=params, cookies=cd)
-
-    verifyResponse(response)
-
-    nh_balance = float(response.json()['confirmed'])
+    nh_balance = float(wallet['confirmed'])
     logger.info("Nicehash confirmed balance: %f mBTC", nh_balance*1000)
 
     # Make withdrawl to coinbase
     withdraw_request_id = None
     if(nh_balance > 0.002):
-        wd_headers = {
-            'Origin': 'https://www.nicehash.com',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'en-US,en;q=0.9,nb;q=0.8,sv;q=0.7',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.52 Safari/537.36',
-            'Content-Type': 'application/json',
-            'Accept': '*/*',
-            'Referer': 'https://www.nicehash.com/wallet',
-            'Connection': 'keep-alive',
-            'DNT': '1',
-        }
 
-        wd_data = '{"address":"'+coinbase_account+'","amount":"'+str(nh_balance)+'","type":1}'
-        logger.debug("Making withdrawl with data: %s", wd_data)
         logger.info("Transferring %f BTC, to coinbase account: %s", nh_balance, coinbase_account)
-        wd_resp = requests.post('https://www.nicehash.com/siteapi/wallet/withdraw_create', headers=wd_headers, cookies=cd, data=wd_data)
-        
-        logger.debug(wd_resp.text)
-        # {"withdraw_request_id":733146}
+        resp = api.withdrawal_request(coinbase_account, nh_balance)
 
-        verifyResponse(wd_resp)
-
-        withdraw_request_id = wd_resp.json()['withdraw_request_id']
+        withdraw_request_id = resp['withdraw_request_id']
         logger.info("Sleeping 25 sec to wait for email")
         time.sleep(25)
 
-    if(withdraw_request_id == None and len(response.json()['withdraw_requests']) > 0):
-            withdraw_request_id = response.json()['withdraw_requests'][0]["id"]
+    if(withdraw_request_id == None and len(wallet['withdraw_requests']) > 0):
+        withdraw_request_id = wallet['withdraw_requests'][0]["id"]
 
     # Try to confirm the withdrawal by reading the email
     if(withdraw_request_id != None and gmail_username):
@@ -153,28 +79,9 @@ def spin():
         
         if(key != None):
 
-
             # Confirm withdrawal
-            cf_headers = {
-                'Origin': 'https://www.nicehash.com',
-                'Accept-Encoding': 'gzip, deflate, br',
-                'Accept-Language': 'en-US,en;q=0.9,nb;q=0.8,sv;q=0.7',
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.52 Safari/537.36',
-                'Content-Type': 'application/json',
-                'Accept': '*/*',
-                'Referer': 'https://www.nicehash.com/wallet',
-                'Connection': 'keep-alive',
-                'DNT': '1',
-            }
-
-            cf_data = '{"code":"'+key+'","id":"'+str(withdraw_request_id)+'","twofa":""}'
-            logger.debug("Confirming withdrawal with data: %s", cf_data)
             logger.info("Confirming withdrawal %i, to coinbase account: %s", withdraw_request_id, coinbase_account)
-            cf_resp = requests.post('https://www.nicehash.com/siteapi/wallet/withdraw_confirm', headers=cf_headers, cookies=cd, data=cf_data)
-            
-            logger.debug(cf_resp.text)
-            
-            verifyResponse(cf_resp)
+            api.withdrawal_confirm(withdraw_request_id, key)
 
             
 if __name__ == "__main__":
@@ -182,13 +89,23 @@ if __name__ == "__main__":
     while(True):
         try:
             spin()
-        except NicehashAuthException as e:
+        except nicehash_site_api.NicehashAuthException as e:
             logger.error("Nicehash authentication error, quitting: %s", e)
             logger.info("Waiting %s seconds before exit/restart to avoid fast loops", spin_wait)
             time.sleep(spin_wait)
             raise e
-        except NicehashServerErrorException as e:
+        except nicehash_site_api.NicehashClientErrorException as e:
+            logger.error("Nicehash client error, quitting: %s", e)
+            logger.info("Waiting %s seconds before exit/restart to avoid fast loops", spin_wait)
+            time.sleep(spin_wait)
+            raise e
+        except nicehash_site_api.NicehashServerErrorException as e:
             logger.error("Nicehash server error, retrying: %s", e)
+        except Exception as e:
+            logger.error("Unknown error, quitting: %s", e)
+            logger.info("Waiting %s seconds before exit/restart to avoid fast loops", spin_wait)
+            time.sleep(spin_wait)
+            raise e
             
         logger.info("Sleeping for %i seconds", spin_wait)
         #break
---
+++ nicehash_site_api.py
@@ -0,0 +1,88 @@
+import requests, time, os, sys
+import logging
+
+
+
+class Client:
+
+
+    def __init__(self, cookies):
+        self.cookies = cookies
+
+        self.headers = {
+            'DNT': '1',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9,nb;q=0.8,sv;q=0.7',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.52 Safari/537.36',
+            'Accept': '*/*',
+            'Referer': 'https://www.nicehash.com/wallet',
+            'Connection': 'keep-alive',
+        }
+
+        self.logger = logging.getLogger()
+
+    def wallet(self):
+        
+        params = (
+            ('with', 'transactions,deposits,withdraws,withdraw_requests'),
+        )
+        return self.get('https://www.nicehash.com/siteapi/user/wallet', params=params)
+
+    def withdrawal_request(self, coinbase_account, amount):
+
+        data = '{"address":"'+coinbase_account+'","amount":"'+str(amount)+'","type":1}'
+        return self.post('https://www.nicehash.com/siteapi/wallet/withdraw_create', data=data)
+
+    def withdrawal_confirm(self, withdraw_request_id, key):
+
+        data = '{"code":"'+key+'","id":"'+str(withdraw_request_id)+'","twofa":""}'
+        return self.post('https://www.nicehash.com/siteapi/wallet/withdraw_confirm', data=data)
+
+
+    def get(self, uri, params = None):
+        self.logger.debug("GET: %s with params %s", uri, params)
+        response = requests.get(uri, headers=self.headers, params=params, cookies=self.cookies)
+        self.logger.debug("GET response(%i): %s", response.status_code, response.text)
+        self.verifyResponse(response)
+        
+        return response.json()
+
+
+    def post(self, uri, params = None, data = ""):
+        self.logger.debug("POST: %s with params %s, data: %s", uri, params, data)
+        response = requests.post(uri, headers=self.headers, params=params, cookies=self.cookies, data=data)
+        self.logger.debug("POST response(%i): %s", response.status_code, response.text)
+        self.verifyResponse(response)
+        
+        return response.json()
+        
+    def verifyResponse(self, response):
+        if (response.status_code >= 500):
+            raise NicehashServerErrorException(response)
+        elif(response.status_code >= 400):
+            raise NicehashAuthException(response)
+
+        data = response.json()
+        if ("status_code" in data and data["status_code"] >= 500):
+            raise NicehashServerErrorException(response)
+        elif("status_code" in data and data["status_code"] >= 400):
+            raise NicehashAuthException(response)
+        elif("error_code" in data and int(data["error_code"]) != 0):
+            raise NicehashClientErrorException(response)
+        elif("error" in data and int(data["error"]) != 0):
+            raise NicehashServerErrorException(response)
+
+class NicehashException(Exception):
+    def __init__(self, response):
+        self.response = response
+        self.status_code = response.status_code
+    def __str__(self):
+        return 'NicehashException: %s %s' % (str(self.status_code), self.response.text)
+
+class NicehashAuthException(NicehashException): pass
+class NicehashServerErrorException(NicehashException): pass
+class NicehashClientErrorException(NicehashException): pass
+class NicehashUnknownErrorException(NicehashException): pass
+
+
+
